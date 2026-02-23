4a8863e85cdaf4da4073747da12f8378210029da
Ahson Shaikh <ahsonshaikh616@gmail.com>
2026-02-23 23:37:17 +0500

    Fixed Waha

--- templates/waha/index.ts
+++ templates/waha/index.ts
@@ -1,8 +1,11 @@
-import { Output, Services } from "~templates-utils";
+import { Output, randomString, Services } from "~templates-utils";
 import { Input } from "./meta";
 
 export function generate(input: Input): Output {
   const services: Services = [];
+
+  const apiKey = randomString(32);
+  const dashboardPassword = randomString(32);
 
   services.push({
     type: "app",
@@ -18,6 +21,13 @@ export function generate(input: Input): Output {
           port: 3000,
         },
       ],
+      env: [
+        `WAHA_API_KEY=${apiKey}`,
+        `WAHA_DASHBOARD_USERNAME=${input.dashboardUsername}`,
+        `WAHA_DASHBOARD_PASSWORD=${dashboardPassword}`,
+        `WHATSAPP_SWAGGER_USERNAME=${input.swaggerUsername}`,
+        `WHATSAPP_SWAGGER_PASSWORD=${dashboardPassword}`,
+      ].join("\n"),
       mounts: [
         {
           type: "volume",
